4ce03b57b4814929f4bddffb82558568e3b73363
Marc Carré <carre.marc@gmail.com>
2024-09-15 17:56:21 +0900

    feat: publish s2a v0.4.0

--- Formula/s/s2a.rb
+++ Formula/s/s2a.rb
@@ -1,8 +1,8 @@
 class S2a < Formula
   desc "Tool to convert a SSH configuration to an Ansible YAML inventory"
   homepage "https://github.com/marccarre/ssh-to-ansible"
-  url "https://github.com/marccarre/ssh-to-ansible/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "b024c669d3cc232e86066f2205546a95997021e7acce03f98ed317e8fd913f38"
+  url "https://github.com/marccarre/ssh-to-ansible/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "fac09310ae9ad726f0ce7fa53a6ab211cdf7d5e1d491226a9a4af4a253619265"
   license "Apache-2.0"
   head "https://github.com/marccarre/ssh-to-ansible.git", branch: "main"
 
@@ -34,7 +34,7 @@ class S2a < Formula
     EOF
 
     expected_output = <<~EOF
-      local:
+      dev:
         hosts:
           default:
             ansible_host: 127.0.0.1
@@ -42,8 +42,18 @@ class S2a < Formula
             ansible_user: vagrant
             ansible_ssh_private_key_file: /tmp/.vagrant/machines/default/private_key
             ansible_ssh_extra_args: -o HostKeyAlgorithms=+ssh-rsa -o IdentitiesOnly=yes -o LogLevel=FATAL -o PasswordAuthentication=no -o PubkeyAcceptedKeyTypes=+ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+        vars:
+          become: true
+          http_port: '8080'
+          num_workers: 4
+          user: root
     EOF
 
-    assert_equal expected_output, pipe_output("#{bin}/s2a", test_ssh_config)
+    assert_equal expected_output, pipe_output("#{bin}/s2a " \
+                                              "-e dev " \
+                                              "--var become:true " \
+                                              "--var http_port:\"'8080'\" " \
+                                              "--var num_workers:4 " \
+                                              "--var user:root", test_ssh_config)
   end
 end
